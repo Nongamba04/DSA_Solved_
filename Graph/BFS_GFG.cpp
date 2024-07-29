ac7a4bd009f2c6a3c8ead7a2584bf084af9d97f4
Its_Nongamba <121596364+Nongamba04@users.noreply.github.com>
2024-07-29 22:39:13 +0530

    Create BFS_GFG.cpp

---
+++ Graph/BFS_GFG.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        // Code here
+        queue<int>q;
+        vector<int>visited(V,0);
+        vector<int> ans;
+        
+        visited[0] = 1;
+        q.push(0);
+        
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            
+            for(auto it : adj[node]){
+                if(visited[it] != 1){
+                    visited[it] = 1;
+                    q.push(it);
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
